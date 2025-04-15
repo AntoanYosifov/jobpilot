0d7bf6fd8d347bb84f36451ddcc650422e57ec96
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-04-15 11:15:25 +0200

    Initial commit

---
+++ jobpilot/src/test/java/com/antdevrealm/jobpilot/JobpilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antdevrealm.jobpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobpilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
