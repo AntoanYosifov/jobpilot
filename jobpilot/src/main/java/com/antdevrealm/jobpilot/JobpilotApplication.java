14b31208275915666a3cb01720b718dac9e2c20b
AntoanYosifov <tapaktapxaomi@gmail.com>
2025-04-16 10:32:44 +0200

    Initial entity setup with placeholder fields for JobApplication model; PostgreSQL integration configured

--- jobpilot/src/main/java/com/antdevrealm/jobpilot/JobpilotApplication.java
+++ jobpilot/src/main/java/com/antdevrealm/jobpilot/JobpilotApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JobpilotApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(JobpilotApplication.class, args);
     }
